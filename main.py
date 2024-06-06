651123a428828a2c99b9a6f1beee8eedd0911428
JusticeTomlinson <90236334+JusticeTomlinson@users.noreply.github.com>
2024-06-05 21:24:42 -0400

    Data Validation Complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.mlops_project import logger
 from src.mlops_project.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
+from src.mlops_project.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
 
 logger.info("Starting logs.")
 
@@ -9,6 +10,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
